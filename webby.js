b781e94708c6cd904a5711bc48a283c5dafc5219
Liam <calendar@dickbailey.com>
2024-06-24 10:43:44 -0400

    revert

--- webby.js
+++ webby.js
@@ -46,14 +46,12 @@ const createMondayItem = async (itemName, columnValues) => {
             create_item (
                 board_id: ${MONDAY_BOARD_ID},
                 item_name: "${itemName}",
-                column_values: ${JSON.stringify(columnValues).replace(/"/g, '\\"')}
+                column_values: "${columnValues.replace(/\\/g, '\\\\').replace(/"/g, '\\"')}"
             ) {
                 id
             }
         }
     `;
-
-    console.log('GraphQL Query:', query); // Log the query for debugging
 
     try {
         const response = await axios.post('https://api.monday.com/v2', { query }, {
@@ -77,41 +75,39 @@ app.post('/webhook', (req, res) => {
 
     let subject, text, columnValues;
     columnValues = {
-        email1: { email: data.user_email, text: data.user_name },
-        date1: { date: data.timestamp },
-        text1: data.account_slug,
-        text2: data.account_name,
-        text3: data.app_id,
-        text4: data.user_cluster,
-        status1: { label: data.account_tier },
-        text5: data.account_max_users,
-        text6: data.account_id,
-        text7: data.plan_id,
-        text8: data.user_country
+        email1: JSON.stringify({ email: data.user_email, text: data.user_name }),
+        date1: JSON.stringify({ date: data.timestamp }),
+        text1: JSON.stringify(data.account_slug),
+        text2: JSON.stringify(data.account_name),
+        text3: JSON.stringify(data.app_id),
+        text4: JSON.stringify(data.user_cluster),
+        status1: JSON.stringify({ label: data.account_tier }), // Ensure this is properly formatted for status
+        text5: JSON.stringify(data.account_max_users),
+        text6: JSON.stringify(data.account_id),
+        text7: JSON.stringify(data.plan_id),
+        text8: JSON.stringify(data.user_country)
     };
-
-    console.log('Column Values:', columnValues); // Log column values for debugging
 
     switch (notificationType) {
         case 'install':
             subject = 'New App Installation';
             text = `A new user has installed your app:\n${JSON.stringify(data, null, 2)}`;
-            createMondayItem(data.user_name, columnValues);
+            createMondayItem(data.user_name, JSON.stringify(columnValues));
             break;
         case 'app_subscription_created':
             subject = 'New App Subscription Created';
             text = `A new subscription has been created:\n${JSON.stringify(data, null, 2)}`;
-            createMondayItem(data.user_name, columnValues);
+            createMondayItem(data.user_name, JSON.stringify(columnValues));
             break;
         case 'app_subscription_changed':
             subject = 'App Subscription Changed';
             text = `A subscription has been changed:\n${JSON.stringify(data, null, 2)}`;
-            createMondayItem(data.user_name, columnValues);
+            createMondayItem(data.user_name, JSON.stringify(columnValues));
             break;
         case 'app_trial_subscription_started':
             subject = 'App Trial Subscription Started';
             text = `A trial subscription has started:\n${JSON.stringify(data, null, 2)}`;
-            createMondayItem(data.user_name, columnValues);
+            createMondayItem(data.user_name, JSON.stringify(columnValues));
             break;
         default:
             res.sendStatus(200); // Ignore other events
